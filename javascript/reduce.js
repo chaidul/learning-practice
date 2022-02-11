c63b4cf5d18872872b0b4650f36f249be4952775
chaidul <technicalgyanassam@gmail.com>
2022-02-11 17:52:01 +0530

    add Reduce function topic

---
+++ javascript/reduce.js
@@ -0,0 +1,8 @@
+/*Reduce function*/
+/*Reduce function help all array sum*/
+var array = [1,2,3,4,5,6,7,8,9];
+const result = array.reduce((total,arr)=>{
+	return total + arr
+},0)
+console.log(result)
+
