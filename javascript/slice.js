bb68c0ff37ca4434cdaeed881a06c5db0b18b200
chaidul <technicalgyanassam@gmail.com>
2022-02-11 21:53:20 +0530

    add array slice method topic

---
+++ javascript/slice.js
@@ -0,0 +1,7 @@
+/*slice method help cut number from starting*/
+var array = [1,2,3,4,5,6,7,8,9];
+/*cut starting 3 number in array*/
+const result = array.slice(3);
+console.log(result);
+
+/*cut starting number only in array & array of object*/
