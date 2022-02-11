7ca9c095a4280c3571a37f40e5d0654bdf9efc03
chaidul <technicalgyanassam@gmail.com>
2022-02-11 15:47:07 +0530

    add map function topic

---
+++ javascript/map.js
@@ -0,0 +1,11 @@
+/*map function topic*/
+var array = [1,2,3,4,5,6,7,8,9,0];
+console.log(`original array : ${array}`);
+/*example 1*/
+const result = array.map(x=>x*x)
+console.log(result)
+/*example 2*/
+const result2=array.map(x=>x+2)
+console.log(result2)
+/*map function help output calculation array*/
+
