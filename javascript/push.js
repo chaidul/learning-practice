7d16e6e69c10c3040aaf5d750996aaf5f7f38438
chaidul <technicalgyanassam@gmail.com>
2022-02-11 21:39:18 +0530

    add push method topic in javascript

---
+++ javascript/push.js
@@ -0,0 +1,16 @@
+/*Push method topic*/
+/*push with array*/
+var arr = [1,2,3,4,5,6,7,8];
+arr.push(9);
+console.log(arr);
+
+/*push with object*/
+var obj={
+	"id":1,
+	"name":"chaidul",
+	"profession":"software developer",
+	
+}
+obj["city"]="guwahati";
+console.log(obj);
+/*thank you for reading*/
