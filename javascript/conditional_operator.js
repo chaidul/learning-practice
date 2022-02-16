c274271e91ca8a0a72f56a59c868701f6bcc83b5
Chaidul <technicalgyanassam@gmail.com>
2022-02-16 11:45:46 +0530

    conditional operator topic

---
+++ javascript/conditional_operator.js
@@ -0,0 +1,10 @@
+/*conditional operator topic in js */
+/*
+variable =(condition)?"true statement" : "false statement"
+
+*/
+const result = (5>6)?"True result":"False result"
+console.log(result)
+
+const result2 = (7<9)?"True result":"False result"
+console.log(result2)
