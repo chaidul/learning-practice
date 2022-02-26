4f47467dfcc26d778afc242c60268d70936a8757
Chaidul <technicalgyanassam@gmail.com>
2022-02-26 10:21:14 +0530

    reverse of a string using recursion in js

---
+++ dsa-cp/recursion_reverse_string.js
@@ -0,0 +1,16 @@
+/*
+reverse of a string using recursion in js
+chaidul=ludiahc
+*/
+const reverseHelper = function(s,first,last){
+	if(first>=last) return;
+	var temp = s[first];
+	s[first]=s[last];
+	s[last]=temp
+	reverseHelper(s,first+1,last-1);
+}
+const reverseString = function(s){
+	  reverseHelper(s,0,s.length-1);	
+}
+
+reverseString("chaidul");
