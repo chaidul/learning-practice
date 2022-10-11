0987c8559990803655e63612b9a5abd30e4540c1
Chaidul <technicalgyanassam@gmail.com>
2022-10-11 18:47:27 +0530

    Doubly LinkedList Add & Display node in JS

---
+++ dsa-cp/LinkedList_doubly.js
@@ -0,0 +1,52 @@
+/*
+Doubly LinkedList DSA in javascript
+*/
+class Node{
+	constructor(data){		
+		this.data=data;
+		this.prev=null;
+		this.next=null;
+	}
+	
+}
+class Linkedlist{
+	constructor(){
+		this.head=null;
+		this.tail=null;
+		this.size=null;
+	}
+	
+	add(data){
+		var node = new Node(data)
+		var last = this.head
+		node.next = null
+		if(this.head==null){
+			node.prev = null
+			this.head=node
+			return
+		}
+		while(last.next != null){
+			last = last.next
+		}
+		last.next = node
+		node.prev = last
+	}
+
+
+
+	print(head=this.head){
+		while(head != null){
+			console.log(head.data)
+			head = head.next
+		}
+	}
+	
+}
+
+const ll = new Linkedlist
+ll.add(1)
+ll.add(2)
+ll.add(3)
+ll.add(4)
+ll.add(5)
+ll.print()
