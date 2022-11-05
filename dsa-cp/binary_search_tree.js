0e66133d8ed1214bcc27a005f20ae394bdc5605e
Chaidul <technicalgyanassam@gmail.com>
2022-11-05 19:28:28 +0530

    BST delete Node using recursion in javascript

--- dsa-cp/binary_search_tree.js
+++ dsa-cp/binary_search_tree.js
@@ -45,6 +45,35 @@ class BST{
 		}
 		return this.search(root.right,x)
 	}
+
+
+	findMin(node){
+		while(node.left != null){
+			node = node.left
+		}
+		return node
+	}
+
+	deleteNode(root,key){
+		if(root == null) return null;
+		if(key < root.data){
+			root.left = this.deleteNode(root.left,key)
+		}else if(key > root.data){
+			root.right = this.deleteNode(root.right,key)
+		}else{
+				if(root.left == null){
+					return root.right
+				}else if(root.right == null){
+					return root.left
+				}
+
+				var minNode = this.findMin(root.right)
+				root.data = minNode
+				root.right = this.deleteNode(root.right,key)
+							
+		}
+		return root
+	}
 	
 }
 
@@ -60,6 +89,9 @@ bst.insert(root,7)
 bst.insert(root,8)
 bst.insert(root,9)
 
+bst.deleteNode(root,5)
 /*BST inOrder traversal always display sorted nodes*/
 bst.inOrder(root)
-bst.search(root,4)
+
+//bst.search(root,4)
+
