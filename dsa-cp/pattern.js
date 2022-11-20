69e8a3dd18702ab3bcc5b1c94da15ad02289836e
Chaidul <technicalgyanassam@gmail.com>
2022-11-20 20:13:39 +0530

    Hollow star pattern in javascript

--- dsa-cp/pattern.js
+++ dsa-cp/pattern.js
@@ -12,4 +12,20 @@ function square(row,col){
 	}
 }
 
-square(8,8)
+//square(8,8)
+
+function hollow(row,col){
+	for(var i=0;i<row;i++){
+		let star = ""
+		for(var j=0;j<col;j++){
+			if(i==0 || j==0 || i==row-1 || j==col-1){
+				star +="*"
+			}else{
+				star +=" "
+			}
+		}
+		console.log(star)
+	}
+}
+
+hollow(6,6)
