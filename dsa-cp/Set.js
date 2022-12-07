d60414f62317b81c5ef2b0b739eabd67a7ae8096
Chaidul <technicalgyanassam@gmail.com>
2022-12-07 11:04:12 +0530

    Set object in hashset in javascript

---
+++ dsa-cp/Set.js
@@ -0,0 +1,19 @@
+/*
+Hashset function javascript using Set
+
+*/
+
+var set = new Set();
+set.add(5)
+set.add(6)
+set.add(7)
+set.add(8)
+set.add(9)
+set.add(4)
+
+set.delete(4)
+
+//console.log(set.size)
+//console.log(set.has(4))
+
+console.log(set)
