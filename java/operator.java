bc87cc651516ae093852c45ae42020956faf5bad
chaidul <technicalgyanassam@gmail.com>
2023-04-30 19:02:13 +0530

    operator in java

---
+++ java/operator.java
@@ -0,0 +1,18 @@
+// Operator in java
+class Operator{
+    public static void main(String arg[]){
+        System.out.println(5+6);
+        System.out.println(9-4);
+        System.out.println(7*8);
+        System.out.println(2000/50);
+        System.out.println(90%8);
+
+        // bitwise operator
+        System.out.println("bitwise operator");
+        System.out.println(1 | 1);
+        System.out.println(1 & 1);
+        System.out.println(1 ^ 1);
+        System.out.println(1 << 2);
+        System.out.println(2 >> 1);
+    }
+}
