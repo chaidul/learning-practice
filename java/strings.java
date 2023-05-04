451f6b28131729f4bbd8972a0c78802c83eba67b
chaidul <technicalgyanassam@gmail.com>
2023-05-04 13:50:47 +0530

    string in java

---
+++ java/strings.java
@@ -0,0 +1,8 @@
+class Strings{
+    public static void main(String arg[]){
+        String name = "Chaidul";
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
